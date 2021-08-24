bc0471da4477e247f46309659ff7eba7827b5bdf
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-08-23 22:52:05 -0300

    Update/Adding Navbar and Welcome components

--- src/App.js
+++ src/App.js
@@ -8,57 +8,15 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import {Card, CardContent, CardMedia} from '@material-ui/core'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import Welcome from './components/Welcome';
+import Navbar from './components/Navbar';
 
 function App() {
-  const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-
-      <div style={{ backgroundColor: '#368acf', display: 'flex', height: '100vh', alignItems: 'center'}} >    
-        <Card style={{ width: '100%', height: '500px', margin: '2%'}}>
-          <CardMedia
-            component="img"
-            alt="Contemplative Reptile"
-            height="100%"
-            image="https://s1.eestatic.com/2020/10/26/como/flores-hogar-decoracion_531208808_163604389_1706x1280.jpg"
-            title="Contemplative Reptile"
-          />
-        </Card>
-
-        <Card style={{ width: '100%', height: '100px', margin: '2%'}}>
-          <CardContent> 
-            <Typography gutterBottom variant="h5" component="h2">
-                Soy Milena Gimenez
-            </Typography>    
-            <Typography variant="body3" color="textSecondary" component="p">
-              Desarrolladora Web Front-end y Diseñadora Gráfica
-            </Typography>   
-          </CardContent>          
-        </Card>
-      </div>
+    <div>            
+      <Navbar />
+      <Welcome />
     </div>
   );
 }
