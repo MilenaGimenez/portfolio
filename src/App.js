60a7a6ce6ddca270188ce465fcc6d67809c5a3a9
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-08-23 22:41:32 -0300

    Update/Adding welcome description cards

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import {Card, CardContent, CardMedia} from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -35,6 +36,29 @@ function App() {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+
+      <div style={{ backgroundColor: '#368acf', display: 'flex', height: '100vh', alignItems: 'center'}} >    
+        <Card style={{ width: '100%', height: '500px', margin: '2%'}}>
+          <CardMedia
+            component="img"
+            alt="Contemplative Reptile"
+            height="100%"
+            image="https://s1.eestatic.com/2020/10/26/como/flores-hogar-decoracion_531208808_163604389_1706x1280.jpg"
+            title="Contemplative Reptile"
+          />
+        </Card>
+
+        <Card style={{ width: '100%', height: '100px', margin: '2%'}}>
+          <CardContent> 
+            <Typography gutterBottom variant="h5" component="h2">
+                Soy Milena Gimenez
+            </Typography>    
+            <Typography variant="body3" color="textSecondary" component="p">
+              Desarrolladora Web Front-end y Diseñadora Gráfica
+            </Typography>   
+          </CardContent>          
+        </Card>
+      </div>
     </div>
   );
 }
