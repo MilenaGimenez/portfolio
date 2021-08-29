4e0454eb218f483d1f38853fe01744a53da196bf
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-08-29 18:04:16 -0300

    Update/Text cards with transparent bg, adding component bg

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -2,25 +2,27 @@
 import {Card, CardContent, CardMedia} from '@material-ui/core'
 import Typography from '@material-ui/core/Typography';
 
+import './Welcome.sass';
+
 const Welcome = () => {
     return (
-      <div style={{ backgroundColor: '#368acf', display: 'flex', height: '100vh', alignItems: 'center'}} >    
-        <Card style={{ width: '100%', height: '500px', margin: '2%'}}>
+      <div style={{ display: 'flex', height: '100vh', alignItems: 'center'}} className="bg" >    
+        <Card style={{ width: '100%', height: '500px', margin: '2%', boxShadow: 'none'}}>
           <CardMedia
             component="img"
             alt="Contemplative Reptile"
             height="100%"
-            image="https://s1.eestatic.com/2020/10/26/como/flores-hogar-decoracion_531208808_163604389_1706x1280.jpg"
+            image="https://freedesignfile.com/upload/2019/08/Business-woman-at-the-desk-is-working-on-the-laptop-computer-vector.jpg"
             title="Contemplative Reptile"
           />
         </Card>
 
-        <Card style={{ width: '100%', height: '100px', margin: '2%'}}>
+        <Card style={{ width: '100%', height: '20%', margin: '2%', boxShadow: 'none', backgroundColor: 'transparent', display: 'flex', alignItems: 'center', padding: '0 1.5%'}}>
           <CardContent> 
-            <Typography gutterBottom variant="h5" component="h2">
+            <Typography gutterBottom variant="h4" className="title-card">
                 ¡Hola! soy Milena Gimenez
             </Typography>    
-            <Typography variant="body3" color="textSecondary" component="p">
+            <Typography variant="h5" className="subtitle-card">
               Desarrolladora Web Front-end y Diseñadora Gráfica
             </Typography>   
           </CardContent>          
