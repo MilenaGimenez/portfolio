16c54e63dcb34fc70d0d2c1122602835dd01288c
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-08-29 17:22:52 -0300

    Update/Deleting Studies component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Navbar from './components/Navbar';
 import Welcome from './components/Welcome';
 import Presentation from './components/Presentation';
-import Studies from './components/Studies';
 import GraphicDesign from './components/GraphicDesign';
 import Frontend from './components/Frontend';
 import Contact from './components/Contact';
@@ -13,7 +12,6 @@ function App() {
       <Navbar />
       <Welcome />
       <Presentation />
-      <Studies />
       <GraphicDesign />
       <Frontend />
       <Contact />
