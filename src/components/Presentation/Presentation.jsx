8de7a1e680655a1231316ed1017f6f23256d0cdf
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-08-29 18:38:31 -0300

    Update/Adding about text

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -1,29 +1,36 @@
 import {Card, CardContent, CardMedia, Typography} from '@material-ui/core'
+import './Presentation.sass';
 
 const Presentation = () => {
     return (
-        <div style={{ backgroundColor: '#cf4036', display: 'flex', height: '100vh', alignItems: 'center'}} >    
-        <Card style={{ width: '100%', height: '500px', margin: '2%'}}>
-          <CardMedia
-            component="img"
-            alt="Contemplative Reptile"
-            height="100%"
-            image="https://s1.eestatic.com/2020/10/26/como/flores-hogar-decoracion_531208808_163604389_1706x1280.jpg"
-            title="Contemplative Reptile"
-          />
-        </Card>
+      <div style={{ display: 'flex', height: '100vh', alignItems: 'center', backgroundColor: '#ff9a28'}} >    
+      <Card style={{ width: '100%', height: '500px', margin: '2%', boxShadow: 'none'}}>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="100%"
+          image="https://freedesignfile.com/upload/2019/08/Business-woman-at-the-desk-is-working-on-the-laptop-computer-vector.jpg"
+          title="Contemplative Reptile"
+        />
+      </Card>
 
-        <Card style={{ width: '100%', height: '100px', margin: '2%'}}>
-          <CardContent> 
-            <Typography gutterBottom variant="h5" component="h2">
-                ¡Hola! Esta es la presentación
-            </Typography>    
-            <Typography variant="body3" color="textSecondary" component="p">
-              Desarrolladora Web Front-end y Diseñadora Gráfica
-            </Typography>   
-          </CardContent>          
-        </Card>
-      </div>
+      <Card style={{ width: '100%', height: '100%', margin: '2%', boxShadow: 'none', backgroundColor: 'transparent', display: 'flex', alignItems: 'center', padding: '0 1.5%'}}>
+        <CardContent> 
+          <Typography gutterBottom variant="h4" className="title-card-com">
+              Sobre mí
+          </Typography>    
+          <Typography variant="h5" className="subtitle-card-com">
+            <p>Tengo 23 años y soy de Córdoba, Argentina.</p>
+            <p>Me encanta todo lo relacionado a lo digital y tecnológico.</p>
+            <p>Estudié Diseño Gráfico Publicitario en la <a href="https://upc.edu.ar/"><strong>Universidad Provincial de Córdoba.</strong></a></p>
+            <p>Continué mis estudios formandome como Desarrolladora Web Frontend en <a href="https://adaitw.org/"><strong>Ada ITW</strong></a>.</p>
+            <p>Hoy en día sigo aprendiendo de manera autodidacta para enriquecer mis conocimientos y aprender nuevos saberes tecnológicos!</p>
+            
+
+          </Typography>   
+        </CardContent>          
+      </Card>
+     </div>
     );
 };
 
