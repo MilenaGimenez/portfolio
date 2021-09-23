e0892ce3fecfabad385a4cd1ec11c680d79c9efb
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-09-23 16:15:51 -0300

    Update/Welcome background finished

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -7,10 +7,6 @@ import './Welcome.sass';
 const Welcome = () => {
     return (
       <div style={{ display: 'flex', height: '100vh', alignItems: 'center'}}  className="bg-welcome"  >    
-      <svg height="1000" width="100%" style={{position: 'absolute'}}>
-        <circle cx="170" cy="170" r="120" fill="#3F51B5" className="purple"/>
-        <circle cx="250" cy="250" r="70" fill="#FF9A28" className="orange" />
-      </svg> 
         <Card style={{ width: '100%', height: '500px', margin: '2%', boxShadow: 'none'}}>
           <CardMedia
             component="img"
